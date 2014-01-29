2139c0de318d6d35467a8a1a989aeb6e3929c050
Tobi Hahn <cmpgk1024@gmail.com>
2014-01-29 14:39:06 -0500

    mac osx package

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd src
 
-clang++ *.cpp -g -Wall -I/Users/thahn/utils/homebrew/include/SDL -D_GNU_SOURCE=1 -D_THREAD_SAFE -L/Users/thahn/utils/homebrew/lib -lSDLmain -lSDL -Wl,-framework,Cocoa -I/Users/thahn/codestuff/life/include -o ../bin/life
+clang++ *.cpp SDLMain.m -g -Wall -I/Library/Frameworks/SDL.framework/Headers -D_GNU_SOURCE=1 -D_THREAD_SAFE -framework SDL -Wl,-framework,Cocoa -I/Users/thahn/codestuff/life/include -o ../bin/life
+cd ../bin
+cp life octogenarian.app/Contents/MacOS
