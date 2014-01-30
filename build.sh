99fb167e2bebc09d3e365bd1ef6cfb0a410a18d8
Tobi Hahn <cmpgk1024@gmail.com>
2014-01-30 06:35:27 -0500

    fixed install names

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ cd src
 
 clang++ *.cpp SDLMain.m -g -Wall -I/Library/Frameworks/SDL.framework/Headers -D_GNU_SOURCE=1 -D_THREAD_SAFE -framework SDL -Wl,-framework,Cocoa -I/Users/thahn/codestuff/life/include -o ../bin/life
 cd ../bin
+install_name_tool -add_rpath @executable_path/../Frameworks life
 cp life octogenarian.app/Contents/MacOS
